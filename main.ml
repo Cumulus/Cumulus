45367734d6684a4ac74ec669a7463e09b2709c9c
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-06-09 05:39:28 +0200

    General code cleaning

--- main.ml
+++ main.ml
@@ -1,17 +1,17 @@
-open Eliom_pervasives.HTML5
+module Html = Eliom_pervasives.HTML5
 
 let main_service =
   Eliom_output.Html5.register_service
-  ~path: [""]
-  ~get_params: Eliom_parameters.unit
-  (fun () () ->
-    Lwt.return
-    (html
-      (head (title (pcdata "Hello World")) [])
-      (body [
-        p [
-          pcdata "Hello World"
-        ]
-      ])
+    ~path: [""]
+    ~get_params: Eliom_parameters.unit
+    (fun () () ->
+      Lwt.return
+        (Html.html
+           (Html.head (Html.title (Html.pcdata "Hello World")) [])
+           (Html.body [
+             Html.p [
+               Html.pcdata "Hello World"
+             ]
+           ])
+        )
     )
-  )
