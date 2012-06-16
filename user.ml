7bcf2762835067091e85dbc8a20b96ac0745cab8
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-06-16 17:22:45 +0200

    Check if is not already connected before connecting

--- user.ml
+++ user.ml
@@ -1,4 +1,5 @@
 type user = (string option) Eliom_reference.eref
+type user_state = Already_connected | Ok | Bad_password
 
 let current_user =
   Eliom_reference.eref ~scope: Eliom_common.session None
@@ -17,7 +18,13 @@ let user_is_connected self =
 
 let user_connect self username password =
   (* TODO: If passwd match *)
-  Eliom_reference.set self (Some username)
+  user_is_connected self >>= (fun state ->
+    match state with
+      | true -> Lwt.return Already_connected
+      | false -> Eliom_reference.set self (Some username) >>= (fun () ->
+        Lwt.return Ok
+      )
+  )
 
 let user_disconnect self =
   user_is_connected self >>= (fun state ->
--- user.mli
+++ user.mli
@@ -1,7 +1,8 @@
 type user
+type user_state = Already_connected | Ok | Bad_password
 
 val user_new : unit -> user
 (* TODO: Change the return type *)
 val user_is_connected : user -> bool Lwt.t
-val user_connect : user -> string -> string -> unit Lwt.t
+val user_connect : user -> string -> string -> user_state Lwt.t
 val user_disconnect : user -> bool Lwt.t
