5a91662509ae206cf71ae02b32f716d20346eacd
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-06-28 19:15:51 +0200

    Add the CSS

--- src/templates.ml
+++ src/templates.ml
@@ -2,41 +2,53 @@ let private_main msg feeds =
   feeds >>= (fun feeds ->
     Lwt.return
       (Html.html
-         (Html.head (Html.title (Html.pcdata "Cumulus")) [])
-         (Html.body
-            (msg @ feeds @ [
-              Html.get_form Services.append_feed
-                (fun (url_name, title_name) -> [
-                  Html.p [
-                    Html.string_input
-                      ~input_type: `Text
-                      ~name: url_name ();
-                    Html.string_input
-                      ~input_type: `Text
-                      ~name: title_name ();
-                    Html.string_input
-                      ~input_type: `Submit
-                      ~value: "Send" ()
-                  ]
-                ]);
-              Html.post_form Services.auth
-                (fun (user_name, password_name) -> [
-                  Html.p [
-                    Html.string_input
-                      ~input_type: `Text
-                      ~name: user_name ();
-                    Html.string_input
-                      ~input_type: `Text
-                      ~name: password_name ();
-                    Html.string_input
-                      ~input_type: `Submit
-                      ~value: "Login" ()
-                  ]
-                ]) ();
-              Html.br ();
-              Html.a Services.registration [Html.pcdata "registration"] ()
-            ])
+         (Html.head
+            (Html.title
+               (Html.pcdata "Cumulus")
+            ) [
+              Html.css_link
+                ~uri: (Html.make_uri
+                         ~service: (Eliom_service.static_dir ())
+                         ["style.css"]
+                ) ()
+            ]
          )
+         (Html.body [
+            (Html.div ~a: [Html.a_class ["content"]]
+              (msg @ feeds @ [
+                Html.get_form Services.append_feed
+                  (fun (url_name, title_name) -> [
+                    Html.p [
+                      Html.string_input
+                        ~input_type: `Text
+                        ~name: url_name ();
+                      Html.string_input
+                        ~input_type: `Text
+                        ~name: title_name ();
+                      Html.string_input
+                        ~input_type: `Submit
+                        ~value: "Send" ()
+                    ]
+                  ]);
+                Html.post_form Services.auth
+                  (fun (user_name, password_name) -> [
+                    Html.p [
+                      Html.string_input
+                        ~input_type: `Text
+                        ~name: user_name ();
+                      Html.string_input
+                        ~input_type: `Text
+                        ~name: password_name ();
+                      Html.string_input
+                        ~input_type: `Submit
+                        ~value: "Login" ()
+                    ]
+                  ]) ();
+                Html.br ();
+                Html.a Services.registration [Html.pcdata "registration"] ()
+              ])
+            )
+         ])
       )
   )
 
