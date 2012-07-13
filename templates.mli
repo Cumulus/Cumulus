298e882d97bee574f4aeaa615d288bd874d6de3e
Enguerrand Decorne <decorne.en@gmail.com>
2012-07-13 15:15:27 +0200

    Fix a type issue

--- templates.mli
+++ templates.mli
@@ -1,4 +1,4 @@
-val main : [< Html5_types.body_content_fun > `A `Br `Form `P ] Html.elt list ->
+val main : [< Html5_types.body_content_fun > `Br `Footer `Form `P ] Html.elt list ->
   [> `Html ] Html.elt Lwt.t
-val user : [< Html5_types.body_content_fun > `A `Br `Form `P ] Html.elt list ->
+val user : [< Html5_types.body_content_fun > `Br `Footer `Form `P ] Html.elt list ->
   string -> [> `Html ] Html.elt Lwt.t
