74a8596c326bd8da6f716c13168f4c97bf38c1a4
Alexis Cassaigne <ordiclic@ordiclic.eu>
2012-07-15 18:13:23 +0000

    Adding username and password support for the URL checker.

--- utils.ml
+++ utils.ml
@@ -25,10 +25,12 @@ let msg str = [
 
 let is_invalid_url input =
   let regexp_match_url =
-    let legit_chars = "[]0-9A-Za-z_~ ().,+=&%-]" in
-    "^\\(https?\\|ftp\\)://" ^           (* Protocol *)
-      "[A-Za-z0-9.]+" ^                  (* domaine name *)
-      "\\(/"   ^ legit_chars ^ "*\\)*" ^ (* Arborescence *)
-      "\\(\\?" ^ legit_chars ^ "*\\)?" ^ (* Parameters *)
-      "\\(#"   ^ legit_chars ^ "*\\)?$"   (* Anchor *) in
+    let legit_chars = "[]0-9A-Za-z_~().,+=&%-]" in
+    "^\\(https?\\|ftp\\)://" ^              (* Protocol *)
+      "\\(" ^ legit_chars ^ "*?" ^          (* Username *)
+      "\\(:" ^legit_chars ^ "*\\)?@\\)?" ^  (* Password *)
+      "[A-Za-z0-9.]+" ^                     (* Domain name *)
+      "\\(/"   ^ legit_chars ^ "*\\)*" ^    (* Arborescence *)
+      "\\(\\?" ^ legit_chars ^ "*\\)?" ^    (* Parameters *)
+      "\\(#"   ^ legit_chars ^ "*\\)?$"     (* Anchor *) in
   not (Str.string_match (Str.regexp regexp_match_url) input 0)
