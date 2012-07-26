b01118cab6936ca7adea3fc494a1f85827590c13
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-07-26 14:19:35 +0200

    Don't display the user form when an user is connected

--- templates.ml
+++ templates.ml
@@ -20,66 +20,75 @@ let main_style data =
         ])
     )
 
+let user_form state =
+  if not state then [
+    Html.post_form
+      ~a: [Html.a_class ["well form-inline"]]
+      ~service: Services.auth
+      (fun (user_name, password_name) -> [
+        Html.p [
+          Html.pcdata "Nickname ";
+          Html.string_input
+            ~a: [Html.a_class ["input-small"]]
+            ~input_type: `Text
+            ~name: user_name ();
+          Html.pcdata " Password ";
+          Html.string_input
+            ~a: [Html.a_class ["input-small"]]
+            ~input_type: `Password
+            ~name: password_name ();
+          Html.string_input
+            ~a: [Html.a_class ["btn btn-primary"]]
+            ~input_type: `Submit
+            ~value: "Login" ();
+          Html.pcdata "Pas de compte ? ";
+          Html.a
+            Services.registration
+            [Html.pcdata "S'inscrire."] ()
+        ]
+      ]) ()
+  ]
+  else [
+  ]
+
 let private_main msg feeds =
   feeds >>= (fun feeds ->
-    main_style
-      (msg @ feeds @ [
-        Html.post_form
-          ~a: [Html.a_class ["well form-inline"]]
-          ~service: Services.append_feed
-          (fun (url_name, (title_name, tags_name)) -> [
-            Html.p [
-              Html.pcdata "URL ";
-              Html.string_input
-                ~a: [Html.a_class ["input-medium search-query"]]
-                ~input_type: `Text
-                ~name: url_name ();
-              Html.pcdata " Titre du post ";
-              Html.string_input
-                ~a: [Html.a_class ["input-medium search-query"]]
-                ~input_type: `Text
-                ~name: title_name ();
-              Html.pcdata " Tags ";
-              Html.string_input
-                ~a: [Html.a_class ["input-medium search-query"]]
-                ~input_type: `Text
-                ~name: tags_name ();
-              Html.string_input
-                ~a: [Html.a_class ["btn btn-primary"]]
-                ~input_type: `Submit
-                ~value: "Send" ()
-            ]
-          ]) ();
-        Html.post_form
-          ~a: [Html.a_class ["well form-inline"]]
-          ~service: Services.auth
-          (fun (user_name, password_name) -> [
-            Html.p [
-              Html.pcdata "Nickname ";
-              Html.string_input
-                ~a: [Html.a_class ["input-small"]]
-                ~input_type: `Text
-                ~name: user_name ();
-              Html.pcdata " Password ";
-              Html.string_input
-                ~a: [Html.a_class ["input-small"]]
-                ~input_type: `Password
-                ~name: password_name ();
-              Html.string_input
-                ~a: [Html.a_class ["btn btn-primary"]]
-                ~input_type: `Submit
-                ~value: "Login" ();
-              Html.pcdata "Pas de compte ? ";
-              Html.a
-                Services.registration
-                [Html.pcdata "S'inscrire."] ()
-            ]
-          ]) ();
-        Html.br ();
-        Html.footer ~a: [Html.a_class ["footer"]] [
-          Html.pcdata "Cumulus project";
-        ]
-      ])
+    User.is_connected () >>= (fun state ->
+      main_style
+        (msg @ feeds @ [
+          Html.post_form
+            ~a: [Html.a_class ["well form-inline"]]
+            ~service: Services.append_feed
+            (fun (url_name, (title_name, tags_name)) -> [
+              Html.p [
+                Html.pcdata "URL ";
+                Html.string_input
+                  ~a: [Html.a_class ["input-medium search-query"]]
+                  ~input_type: `Text
+                  ~name: url_name ();
+                Html.pcdata " Titre du post ";
+                Html.string_input
+                  ~a: [Html.a_class ["input-medium search-query"]]
+                  ~input_type: `Text
+                  ~name: title_name ();
+                Html.pcdata " Tags ";
+                Html.string_input
+                  ~a: [Html.a_class ["input-medium search-query"]]
+                  ~input_type: `Text
+                  ~name: tags_name ();
+                Html.string_input
+                  ~a: [Html.a_class ["btn btn-primary"]]
+                  ~input_type: `Submit
+                  ~value: "Send" ()
+              ]
+            ]) ()
+         ] @ (user_form state) @ [
+          Html.br ();
+          Html.footer ~a: [Html.a_class ["footer"]] [
+            Html.pcdata "Cumulus project";
+          ]
+        ])
+    )
   )
 
 let private_register =
