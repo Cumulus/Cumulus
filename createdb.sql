83feac12e36bcda21f4c84db2797a5fd804816ee
Romain Calascibetta <romain.calascibetta@epitech.eu>
2012-10-19 10:31:01 +0000

    Add is_admin (cc jpdeplaix) for 500 internal error

--- createdb.sql
+++ createdb.sql
@@ -19,7 +19,8 @@ CREATE TABLE users (
   id integer NOT NULL,
   name text NOT NULL,
   password text NOT NULL,
-  email text NOT NULL
+  email text NOT NULL,
+  is_admin boolean NOT NULL DEFAULT(false)
 );
 
 CREATE SEQUENCE users_id_seq;
