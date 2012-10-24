a0bcbae014283226cd9cd569c91f352d5651aef4
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-10-24 11:23:05 +0200

    Fix the interface of Templates

--- templates.mli
+++ templates.mli
@@ -1,14 +1,18 @@
-val main : ?page:int ->
-  [< Html5_types.div_content_fun > `A `Br `Div `Footer `Header `P ]
-  Html.elt list -> [> `Html ] Html.elt Lwt.t
-val user : ?page:int ->
-  [< Html5_types.div_content_fun > `A `Br `Div `Footer `Header `P ]
-  Html.elt list -> string -> [> `Html ] Html.elt Lwt.t
-val tag : ?page:int ->
-  [< Html5_types.div_content_fun > `A `Br `Div `Footer `Header `P ]
-  Html.elt list -> string -> [> `Html ] Html.elt Lwt.t
+val main :
+  ?page:int ->
+  service:'a ->
+  unit ->
+  [> `Html ] Html.elt Lwt.t
+val user :
+  ?page:int ->
+  service:'a ->
+  string ->
+  [> `Html ] Html.elt Lwt.t
+val tag :
+  ?page:int ->
+  service:'a ->
+  string ->
+  [> `Html ] Html.elt Lwt.t
 val register : unit -> [> `Html ] Html.elt Lwt.t
 val view_feed : int -> [> `Html ] Html.elt Lwt.t
-val preferences :
-  [< Html5_types.div_content_fun > `Form `Header `PCDATA ] Html.elt list ->
-  [> `Html ] Html.elt Lwt.t
+val preferences : unit -> [> `Html ] Html.elt Lwt.t
