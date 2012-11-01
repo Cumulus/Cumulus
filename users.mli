9342fa7ddcaa0e2776891f1a5dda49269865d3c6
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-11-01 11:15:59 +0100

    Remove the module Users: Operations are only for a single user

--- users.mli
+++
@@ -1,4 +0,0 @@
-val connect_user : string -> string -> User.user_state Lwt.t
-val add_user : (string * (string * (string * string))) -> bool Lwt.t
-val update_user_password : (string * string) -> bool Lwt.t
-val update_user_mail : (string) -> bool Lwt.t
