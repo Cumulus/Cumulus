af8f55adadb0897aeea912bfa6f34438396e2c27
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2013-10-29 21:50:35 +0100

    Use Eliom_config to handle the db variables in the config file

--- src/configfile.mli
+++ src/configfile.mli
@@ -19,35 +19,9 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *)
 
-module Lwt_thread = struct
-  include Lwt
-  include Lwt_chan
-end
-module Lwt_PGOCaml = PGOCaml_generic.Make(Lwt_thread)
-module Lwt_Query = Query.Make_with_Db(Lwt_thread)(Lwt_PGOCaml)
-
-let connect =
-  let open Configfile in
-  Lwt_PGOCaml.connect
-    ?database
-    ?host
-    ?port
-    ?password
-    ?user
-    ?unix_domain_socket_dir
-
-let pool = Lwt_pool.create 16 ~validate:Lwt_PGOCaml.alive connect
-
-let use f = Lwt_pool.use pool f
-
-let view x = use (fun db -> Lwt_Query.view db x)
-let view_opt x = use (fun db -> Lwt_Query.view_opt db x)
-let view_one x = use (fun db -> Lwt_Query.view_one db x)
-let query x = use (fun db -> Lwt_Query.query db x)
-let value x = use (fun db -> Lwt_Query.value db x)
-let value_opt x = use (fun db -> Lwt_Query.value_opt db x)
-let alter x = use (fun db -> Lwt_PGOCaml.alter db x)
-
-let rec in' value = function
-  | [] -> Sql.Value.bool false
-  | x::xs -> Sql.Op.(||) (Sql.Op.(=) x value) (in' value xs)
+val database : string option
+val host : string option
+val password : string option
+val user : string option
+val port : int option
+val unix_domain_socket_dir : string option
