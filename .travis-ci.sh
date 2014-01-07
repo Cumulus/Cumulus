2a681901f029a8c0cae7f07787c7b6c84e15c742
Enguerrand Decorne <decorne.en@gmail.com>
2014-01-07 12:35:12 +0100

    typo

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,7 +3,7 @@ OPAM_DEPENDS="ocamlfind cumulus-deps"
 echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam11
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-sude apt-get install libpcre3-dev libssl-dev libsqlite3-dev
+sudo apt-get install libpcre3-dev libssl-dev libsqlite3-dev
 export OPAMYES=1
 opam init
 opam repository add ocsigendev https://github.com/ocsigen/opam-ocsigen.git
