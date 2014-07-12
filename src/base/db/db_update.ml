3fc7051661d96357888d811ce3163eda400a345e
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-07-12 19:11:26 +0200

    Fix update of database

--- src/base/db/db_update.ml
+++ src/base/db/db_update.ml
@@ -82,7 +82,7 @@ let compute_interval_representation () =
           >>= fun rightBound ->
           Db.query
             (<:update< row in $Db_table.feeds$ :=
-                       { leftBound = $int32:leftBound$;
+                       { leftBound = $int32:bound$;
                          rightBound = $int32:rightBound$ }
                        | row.id = $int32:x#!id$ >>)
           >>= fun () -> aux (rightBound + one) r
