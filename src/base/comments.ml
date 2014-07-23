2b329482fa35af19f65f32518085a12a81c8acdc
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-07-23 15:03:02 +0200

    New implementation (more clear) of list of feeds to tree

--- src/base/comments.ml
+++ src/base/comments.ml
@@ -24,9 +24,55 @@ open Eliom_lib.Lwt_ops
 
 module UTF8 = CamomileLibraryDefault.Camomile.CaseMap.Make(CamomileLibrary.UTF8)
 
+type node =
+  { id : int32
+  ; leftBound : int32
+  ; rightBound : int32
+  }
+
 type tree =
   | Sheet of Feed.feed
   | Node of Feed.feed * tree list
+
+let make feed =
+  { id = feed.Feed.id
+  ; leftBound = feed.Feed.leftBound
+  ; rightBound = feed.Feed.rightBound
+  }
+
+let tree_of_node ~user node =
+  let open Int32 in
+  let rec traverse = function
+    | feed :: rest when feed.Feed.leftBound + one = feed.Feed.rightBound ->
+      (rest, Sheet feed)
+    | feed :: rest ->
+      let rec reduce childs = function
+        | feed' :: rest when
+          feed'.Feed.leftBound > feed.Feed.leftBound
+          && feed'.Feed.rightBound < feed.Feed.rightBound ->
+          let (rest, node) = traverse (feed' :: rest) in
+          reduce (node :: childs) rest
+        | rest -> (rest, childs)
+      in let (rest, childs) = reduce [] rest
+      in (rest, Node (feed, List.rev childs))
+    | [] -> assert false
+  in
+  let rec aux acc = function
+    | [] -> List.rev acc
+    | lst ->
+      let (rest, node) = traverse lst
+      in aux (node :: acc) rest
+  in
+  Feed.get_feed_with_id ~user node.id
+  >>= fun root ->
+  Feed.get_feeds_of_interval ~user node.leftBound node.rightBound
+  >>= fun childs ->
+    List.sort
+      (fun feed feed' -> to_int (feed.Feed.leftBound - feed'.Feed.leftBound))
+      (root :: childs)
+    |> Lwt.return
+  >|= aux []
+  >|= List.hd
 
 let get = function
   | Sheet elm
