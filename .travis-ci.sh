92aacb2d9681047f1c4861c40b442c0e62fd0d8c
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-10-08 17:51:37 +0100

    Update the travis script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,8 +4,7 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 sudo apt-get install libpcre3-dev libssl-dev libsqlite3-dev
 export OPAMYES=1
 opam init
-opam repository add ocsigendev https://github.com/ocsigen/opam-ocsigen.git
+eval `opam config env`
 opam repository add cumulus https://github.com/Cumulus/opam-cumulus.git
 opam install cumulus-deps
-eval `opam config env`
 make
