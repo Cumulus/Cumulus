cb7396f98d5d022566f01787ea9623a8a2aad9c5
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-10-23 22:09:45 +0100

    Travis: Use opam 1.2

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam11
+echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam12
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 sudo apt-get install libpcre3-dev libssl-dev
