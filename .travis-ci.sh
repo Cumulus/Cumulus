4e1da1b3d4d192b4659a713c87b7747d1611425c
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-10-23 21:45:57 +0100

    Use travis to build cumulus ocsigenserver 3.0.0 as well

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,10 +1,17 @@
 echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam11
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-sudo apt-get install libpcre3-dev libssl-dev libsqlite3-dev
+sudo apt-get install libpcre3-dev libssl-dev
 export OPAMYES=1
 opam init
 eval `opam config env`
-opam repository add cumulus https://github.com/Cumulus/opam-cumulus.git
+case $OCSIGENSERVER3 in
+    true)
+        opam repository add cumulus https://github.com/Cumulus/opam-cumulus.git;;
+    false)
+        opam repository add cumulus https://github.com/Cumulus/opam-cumulus.git#ocsigenserver-3.0.0;;
+    *)
+        echo Unknown variable '$OCSIGENSERVER3': $OCSIGENSERVER3
+esac
 opam install cumulus-deps
 make
