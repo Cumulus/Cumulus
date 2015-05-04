9fd733fec7e2ee9c5b6e437a3aafddfaf6688583
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-05-04 04:12:19 +0100

    Improve the database setup system

--- createdb.sql
+++ createdb.sql
@@ -1,10 +1,14 @@
-GRANT ALL PRIVILEGES ON DATABASE cumulus TO cumulus;
 SET client_encoding = 'UTF8';
+
+CREATE USER cumulus PASSWORD 'cumulus';
+CREATE DATABASE cumulus OWNER cumulus ENCODING 'UTF8';
+\connect cumulus
 
 CREATE TABLE options (
   name text PRIMARY KEY,
   value text NOT NULL
 );
+ALTER TABLE options OWNER TO cumulus;
 
 CREATE TABLE users (
   id integer PRIMARY KEY,
@@ -14,6 +18,7 @@ CREATE TABLE users (
   is_admin boolean NOT NULL,
   feeds_per_page integer NOT NULL CHECK (feeds_per_page > 0)
 );
+ALTER TABLE users OWNER TO cumulus;
 
 CREATE TABLE feeds (
     id integer PRIMARY KEY,
@@ -26,24 +31,31 @@ CREATE TABLE feeds (
     leftBound integer NOT NULL,
     rightBound integer NOT NULL CHECK (rightBound > leftBound)
 );
+ALTER TABLE feeds OWNER TO cumulus;
 
 CREATE TABLE feeds_tags (
     tag text NOT NULL,
     id_feed integer NOT NULL REFERENCES feeds (id) ON DELETE CASCADE
 );
+ALTER TABLE feeds_tags OWNER TO cumulus;
 
 CREATE TABLE favs (
   id_user integer NOT NULL REFERENCES users (id),
   id_feed integer NOT NULL REFERENCES feeds (id) ON DELETE CASCADE
 );
+ALTER TABLE favs OWNER TO cumulus;
 
 CREATE TABLE votes (
   id_user integer NOT NULL REFERENCES users (id),
   id_feed integer NOT NULL REFERENCES feeds (id) ON DELETE CASCADE,
   score integer NOT NULL
 );
+ALTER TABLE votes OWNER TO cumulus;
 
 CREATE SEQUENCE users_id_seq;
+ALTER SEQUENCE users_id_seq OWNER TO cumulus;
+
 CREATE SEQUENCE feeds_id_seq;
+ALTER SEQUENCE feeds_id_seq OWNER TO cumulus;
 
 INSERT INTO options VALUES ('dbversion', '10');
